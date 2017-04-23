3bb243eee1ad63d2c73882395b61327692c60e26
serifyasar <serifyasar@gmail.com>
2017-04-23 14:57:56 +0300

    Add project files.

---
+++ entityDbFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(entityDbFirst.Startup))]
+namespace entityDbFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
